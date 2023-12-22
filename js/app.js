188a78638cabcf35b17b7c730bfa22172fe71dbb
Paulo Pereira <paulovvp@outlook.com>
2023-12-21 21:51:01 -0300

    atualização

--- js/app.js
+++ js/app.js
@@ -1,49 +1,90 @@
 let amigos = [];
 
-function adicionar(){
-    //recuperar o nome digitado
+
+function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    amigos.push(amigo.value)
-    //adicionar o nome digitado na lista de amigos
-    if (lista.textContent == ''){
+
+
+    amigos.push(amigo.value);
+
+
+    if (lista.textContent == '') {
         lista.textContent = amigo.value;
-    }else{
+    } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
 
+
     amigo.value = '';
+
+
+    atualizarLista();
+    atualizarSorteio();
 }
 
-function sortear(){
-    embaralha(amigos);
-    let listaSorteio = document.getElementById('lista-sorteio');
 
-    for (let i = 0; i < amigos.length; i++){
-        if(i == amigos.length - 1){
-            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
-        }else{
-            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + '-->' + amigos[i +1] + '<br>';
+function sortear() {
+    embaralhar(amigos);
+
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
         }
     }
 }
 
 
-function embaralha(lista) {
+function excluirAmigo(index) {
+    amigos.splice(index, 1);
+    atualizarLista();
+    atualizarSorteio();
+}
 
+
+function embaralhar(lista) {
     for (let indice = lista.length; indice; indice--) {
-
         const indiceAleatorio = Math.floor(Math.random() * indice);
-
-        // atribuição via destructuring
-        [lista[indice - 1], lista[indiceAleatorio]] = 
-            [lista[indiceAleatorio], lista[indice - 1]];
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
 
-function reiniciar(){
-    document.getElementById('nome-amigo').value = '';
-    document.getElementById('lista-amigos').textContent = '';
-    document.getElementById('lista-sorteio').textContent = '';  
-    amigos = [];  
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
 }
+
+
+function atualizarLista() {
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+
+    for (let i = 0; i < amigos.length; i++) {
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = amigos[i];
+       
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
+    }
+}
+
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+
